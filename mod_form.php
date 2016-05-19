b656d03f17b5d0f264f765b8dd7ae6c6f7b06f20
kasunramkr <milindakasun@live.com>
2016-05-19 07:58:11 +0530

    initial project

---
+++ mod_form.php
@@ -0,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The main pa configuration form
+ *
+ * It uses the standard core Moodle formslib. For more info about them, please
+ * visit: http://docs.moodle.org/en/Development:lib/formslib.php
+ *
+ * @package    mod_pa
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot.'/course/moodleform_mod.php');
+
+/**
+ * Module instance settings form
+ *
+ * @package    mod_pa
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_pa_mod_form extends moodleform_mod {
+
+    /**
+     * Defines forms elements
+     */
+    public function definition() {
+        global $CFG;
+
+        $mform = $this->_form;
+
+        // Adding the "general" fieldset, where all the common settings are showed.
+        $mform->addElement('header', 'general', get_string('general', 'form'));
+
+        // Adding the standard "name" field.
+        $mform->addElement('text', 'name', get_string('paname', 'pa'), array('size' => '64'));
+        if (!empty($CFG->formatstringstriptags)) {
+            $mform->setType('name', PARAM_TEXT);
+        } else {
+            $mform->setType('name', PARAM_CLEANHTML);
+        }
+        $mform->addRule('name', null, 'required', null, 'client');
+        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
+        $mform->addHelpButton('name', 'paname', 'pa');
+
+        // Adding the standard "intro" and "introformat" fields.
+        if ($CFG->branch >= 29) {
+            $this->standard_intro_elements();
+        } else {
+            $this->add_intro_editor();
+        }
+
+        // Adding the rest of pa settings, spreading all them into this fieldset
+        // ... or adding more fieldsets ('header' elements) if needed for better logic.
+
+        $mform->addElement('header', 'pafieldset', get_string('pafieldset', 'pa'));
+		
+		$mform->addElement('static', 'label1', 'Test Case 1 <br/>Input', "<textarea name='input1' style='font-size:15pt;height:130px;width:400px;'></textarea>");
+		$mform->addElement('static', 'label1', 'Output', "<textarea name='output1' style='font-size:15pt;height:130px;width:400px;'></textarea>");
+		
+		$mform->addElement('static', 'label1', 'Test Case 2 <br/>Input', "<textarea name='input2' style='font-size:15pt;height:130px;width:400px;'></textarea>");
+		$mform->addElement('static', 'label1', 'Output', "<textarea name='output2' style='font-size:15pt;height:130px;width:400px;'></textarea>");
+
+		$mform->addElement('static', 'label1', 'Test Case 3 <br/>Input', "<textarea name='input3' style='font-size:15pt;height:130px;width:400px;'></textarea>");
+		$mform->addElement('static', 'label1', 'Output', "<textarea name='output3' style='font-size:15pt;height:130px;width:400px;'></textarea>");
+
+        // Add standard grading elements.
+        $this->standard_grading_coursemodule_elements();
+
+        // Add standard elements, common to all modules.
+        $this->standard_coursemodule_elements();
+
+        // Add standard buttons, common to all modules.
+        $this->add_action_buttons();
+    }
+}
---
+++ view.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Prints a particular instance of pa
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_pa
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Replace pa with the name of your module and remove this line.
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+
+$id = optional_param('id', 0, PARAM_INT); // Course_module ID, or
+$n  = optional_param('n', 0, PARAM_INT);  // ... pa instance ID - it should be named as the first character of the module.
+
+if ($id) {
+    $cm         = get_coursemodule_from_id('pa', $id, 0, false, MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $pa  = $DB->get_record('pa', array('id' => $cm->instance), '*', MUST_EXIST);
+} else if ($n) {
+    $pa  = $DB->get_record('pa', array('id' => $n), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $pa->course), '*', MUST_EXIST);
+    $cm         = get_coursemodule_from_instance('pa', $pa->id, $course->id, false, MUST_EXIST);
+} else {
+    error('You must specify a course_module ID or an instance ID');
+}
+
+require_login($course, true, $cm);
+
+$event = \mod_pa\event\course_module_viewed::create(array(
+    'objectid' => $PAGE->cm->instance,
+    'context' => $PAGE->context,
+));
+$event->add_record_snapshot('course', $PAGE->course);
+$event->add_record_snapshot($PAGE->cm->modname, $pa);
+$event->trigger();
+
+// Print the page header.
+
+$PAGE->set_url('/mod/pa/view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($pa->name));
+$PAGE->set_heading(format_string($course->fullname));
+
+/*
+ * Other things you may want to set - remove if not needed.
+ * $PAGE->set_cacheable(false);
+ * $PAGE->set_focuscontrol('some-html-id');
+ * $PAGE->add_body_class('pa-'.$somevar);
+ */
+
+// Output starts here.
+echo $OUTPUT->header();
+
+// Conditions to show the intro can change to look for own settings or whatever.
+if ($pa->intro) {
+    echo $OUTPUT->box(format_module_intro('pa', $pa, $cm->id), 'generalbox mod_introbox', 'paintro');
+}
+
+// Replace the following lines with you own code.
+
+echo $OUTPUT->heading('Submition');
+echo "<textarea name='source' style='font-size:15pt;height:500px;width:700px;'></textarea><br/>";
+echo "<button type='button'>Submit</button>";
+
+// Finish the page.
+echo $OUTPUT->footer();
